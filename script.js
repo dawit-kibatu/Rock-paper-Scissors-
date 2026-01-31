c801e5fdc4104ed5bfdf2746313840ae4609e8e5
Dawit <208569702+dawit-kibatu@users.noreply.github.com.>
2026-01-31 13:35:57 +0300

    Add: working logic with human and computer choice

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
 function getComputerChoice(){
     let x=Math.floor(Math.random()*3);
+    let computerChoice='rock';
     switch(x){
         case 0:
             console.log('rock')
+            computerChoice='rock';
             break;
         case 1:
             console.log('paper')
+            computerChoice='paper';
             break;
         case 2:
-            console.log('Scissors')
+            console.log('scissors')
+            computerChoice='scissors';
             break;
         default:
             break
     }
+    return computerChoice;
 }
 function getHumanChoice(){
-let choice=prompt("choose my guy")
-choice=choice.toLowerCase()
-if (choice =="rock" || choice=="paper" || choice=="scissors"){
-    console.log(choice)
+let humanChoice=prompt("choose my guy")
+humanChoice=humanChoice.toLowerCase()
+if (humanChoice =="rock" || humanChoice=="paper" || humanChoice=="scissors"){
+    console.log(humanChoice)
 }
 else{
     console.log("no you can't do that")
+    humanChoice="0";
 }
+return humanChoice
 }
+let humanScore=0;
+let computerScore=0;
+function playRound(humanChoice,computerChoice){
+    if( humanChoice==computerChoice){
+        console.log("Draw");
+    }
+    else if(humanChoice=="rock" && computerChoice!="paper"){
+        console.log("human win");
+        humanScore++;
+    }
+    else if(humanChoice=="paper" && computerChoice!="scissors"){
+        console.log("human win");
+        humanScore++;
+    }
+    else if(humanChoice=="scissors" && computerChoice!="rock"){
+        console.log("human win");
+        humanScore++;
+    }
+    else{
+        console.log("Computer wins");
+        computerScore++;
+    }
+}
+let humanSelection=getHumanChoice()
+let computerSelection=getComputerChoice()

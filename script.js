6107edf955250721e4b2af7fdb15b24cd56eb24f
Dawit <208569702+dawit-kibatu@users.noreply.github.com.>
2026-01-31 13:10:14 +0300

    Add human choice function

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ function getComputerChoice(){
             break
     }
 }
+function getHumanChoice(){
+let choice=prompt("choose my guy")
+choice=choice.toLowerCase()
+if (choice =="rock" || choice=="paper" || choice=="scissors"){
+    console.log(choice)
+}
+else{
+    console.log("no you can't do that")
+}
+}
